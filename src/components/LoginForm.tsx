8a987339d7e0d1b6aa65921192cbfd498854571a
Yernur Omirzak <reluckytryhrd@gmail.com>
2025-03-22 23:46:20 +0500

    test

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -16,11 +16,6 @@ export default function LoginForm() {
         setError('');
 
         try {
-            console.log("Hello")
-            console.log(process.env.NEXT_PUBLIC_API_URL)
-            console.log(process.env.NEXT_PUBLIC_API_URL)
-            console.log(process.env.NEXT_PUBLIC_API_URL)
-            console.log("HElloo")
             await loginUser({email, password});
             router.push('/users/profile');
         } catch (err) {
